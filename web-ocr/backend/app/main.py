715b4eb92e70aadb7689ea39225fabb069889437
Gaji <kec4411@gmail.com>
2023-11-09 09:43:04 +0900

    fix cors_oprigin

--- web-ocr/backend/app/main.py
+++ web-ocr/backend/app/main.py
@@ -32,12 +32,11 @@ app = FastAPI()
 origins = [
     "*",
     "localhost:3000",
-    "http://ecs-alb-580115360.ap-northeast-1.elb.amazonaws.com/",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
